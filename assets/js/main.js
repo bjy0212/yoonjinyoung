5e4f787d05265822e6909c356a86e375a20d12ad
slrspa421 <bjy0212@gmail.com>
2020-10-08 19:35:26 +0900

    '.'

--- assets/js/main.js
+++ assets/js/main.js
@@ -430,7 +430,7 @@ var main = (function($) { var _ = {
  							// Set background stuff.
 	 							s.$slideImage
 		 							.css('background-image', '')
-		 							.css('background-position', ($thumbnail.data('position') || 'center'));
+		 							.css('background-position', 'center');
 
 						// Caption.
 							s.$slideCaption = s.$slide.find('.caption');
